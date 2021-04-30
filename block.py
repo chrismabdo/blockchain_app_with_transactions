48748cb19bcd44b4a02f056c28538369c9185e3a
Chris Abdo <christophermabdo@icloud.com>
2021-04-30 16:02:08 +0100

    updated framework

--- block.py
+++ block.py
@@ -40,3 +40,51 @@ for i in range(0, num_of_blocks_to_add):
 
     print("Block #{} has been added to the blockchain!".format(block_to_add.index))
     print("Hash: {}\n".format(block_to_add))
+
+
+miner_address = "q3nf394hjg-random-miner-address-34nf3i4nflkn3oi"
+
+def proof_of_work(last_proof):
+  incrementor = last_proof + 1
+
+  while not (incrementor % 9 == 0 and incrementor % last_proof == 0):
+    incrementor += 1
+  
+  return incrementor
+
+@node.route('/mine', methods = ['GET'])
+def mine():
+
+  last_block = blockchain[len(blockchain) - 1]
+  last_proof = last_block.data['proof-of-work']
+
+  proof = proof_of_work(last_proof)
+
+  this_nodes_transactions.append(
+    {"from": "network", "to": miner_address, "amount": 1}
+  )
+
+  new_block_data = {
+    "proof-of-work": proof,
+    "transactions": list(this_nodes_transactions)
+  }
+  new_block_index = last_block.index + 1
+  new_block_timestamp = this_timestamp = date.datetime.now()
+  last_block_hash = last_block.hash
+
+  this_nodes_transactions[:] = []
+
+  mined_block = Block(
+      new_block_index,
+      new_block_timestamp,
+      new_block_data,
+      last_block_hash
+  )
+  blockchain.append(mined_block)
+
+  return json.dumps({
+      "index": new_block_index,
+      "timestamp": str(new_block_timestamp),
+      "data": new_block_data,
+      "hash": last_block_hash
+  }) + "\n"
